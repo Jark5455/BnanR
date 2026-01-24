fe4a436be03e0e31436cc150a6d6d38c0c8fef13
Yash Rachamalla <rshailu.yash@gmail.com>
2026-01-24 17:32:11 -0500

    reduced vcpkg depends

--- build/main.rs
+++ build/main.rs
@@ -6,13 +6,8 @@ use assets::copy_assets;
 
 fn main() {
 
-    vcpkg::Config::new()
-        .vcpkg_root("target/vcpkg".parse().unwrap())
-        .find_package("sdl3").unwrap();
-
-    vcpkg::Config::new()
-        .vcpkg_root("target/vcpkg".parse().unwrap())
-        .find_package("assimp").unwrap();
+    vcpkg::find_package("sdl3").unwrap();
+    vcpkg::find_package("assimp").unwrap();
 
     println!("cargo:rerun-if-changed=build/main.rs");
     println!("cargo:rerun-if-changed=build/shaders.rs");
