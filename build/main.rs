87df20672c767d24df202831e35ec68fa41c7103
Yash Rachamalla <rshailu.yash@gmail.com>
2026-01-24 16:22:35 -0500

    grab build dependencies using vcpkg

--- build/main.rs
+++ build/main.rs
@@ -5,7 +5,14 @@ use shaders::compile_shaders;
 use assets::copy_assets;
 
 fn main() {
-    
+
+    vcpkg::find_package("sdl3").unwrap();
+    vcpkg::find_package("vulkan").unwrap();
+    vcpkg::find_package("vulkan-memory-allocator").unwrap();
+    vcpkg::find_package("shaderc").unwrap();
+    vcpkg::find_package("assimp").unwrap();
+    vcpkg::find_package("meshoptimizer").unwrap();
+
     println!("cargo:rerun-if-changed=build/main.rs");
     println!("cargo:rerun-if-changed=build/shaders.rs");
     println!("cargo:rerun-if-changed=build/assets.rs");
