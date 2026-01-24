9aa277054845544fe9db236afffad7f8bb2fd9e2
Yash Rachamalla <rshailu.yash@gmail.com>
2026-01-24 17:12:21 -0500

    reduced vcpkg depends

--- build/main.rs
+++ build/main.rs
@@ -7,11 +7,7 @@ use assets::copy_assets;
 fn main() {
 
     vcpkg::find_package("sdl3").unwrap();
-    vcpkg::find_package("vulkan").unwrap();
-    vcpkg::find_package("vulkan-memory-allocator").unwrap();
-    vcpkg::find_package("shaderc").unwrap();
     vcpkg::find_package("assimp").unwrap();
-    vcpkg::find_package("meshoptimizer").unwrap();
 
     println!("cargo:rerun-if-changed=build/main.rs");
     println!("cargo:rerun-if-changed=build/shaders.rs");
